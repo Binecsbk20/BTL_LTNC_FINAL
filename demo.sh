0a359520f69f8f8f4e83bbae968839bfb8db46c6
Binecsbk20 <bientran@MacBook-Pro-cua-Bien.local>
2022-05-13 00:28:13 +0700

    upload_bine

--- demo.sh
+++ demo.sh
@@ -1,1 +1,3 @@
-java --module-path /Users/bientran/BTL/lib/javafx-sdk-18/lib --add-modules javafx.controls,javafx.fxml -jar BTL.jar
+java --module-path /BTL/lib/javafx-sdk-18/lib --add-modules javafx.controls,javafx.fxml -jar BTL.jar
+
+java --module-path /lib/javafx-sdk-18/lib --add-modules javafx.controls,javafx.fxml -jar BTL_F.jar
